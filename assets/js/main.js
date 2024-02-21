7742769d80f1aa10d1bdc59a284b725e81b9276f
MassimoRomano <romano93m@gmail.com>
2024-02-21 19:20:09 +0100

    add copy and fix for else if

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,22 @@ const elementList = document.getElementById("list")
 
 for (let i = 1; i <= 100; i++){  //stamp variable i
 
+
+//add var for multiple of 3 and 5
     if (i % 3 === 0 && i % 5 === 0){
     const multipleSame = document.createElement('li')
     multipleSame.innerText = 'FizzBuzz'
     elementList.append(multipleSame)
     }
 
+//add var for mutiple of 3
     else if ( i % 3 === 0){
         const multipleThree = document.createElement('li')
         multipleThree.innerText = 'FIZZ'
         elementList.append(multipleThree) 
     }
+
+    
 
     
 
