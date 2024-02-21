94dde02fb1dc9ce0e194bde5eaebd5cc6b9b36af
MassimoRomano <romano93m@gmail.com>
2024-02-21 18:40:06 +0100

    add css

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,10 @@ console.log('funziona')
 const elementList = document.getElementById("list")
 //var for the id list
 
-for (let i = 1; i<=100; i++){
+for (let i = 1; i <= 100; i++){
     console.log(i)  //stamp variable i
-    const markupListItem = `<li>${i}</li>`
+    const markupListItem = `<li class="box box-${i}">${i}</li>`
     console.log(markupListItem)
+
+    elementList.insertAdjacentHTML("beforeend", markupListItem)
 }
