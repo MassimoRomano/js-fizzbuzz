6fa103f224939e9004d8d783b2dee7da42d7cfe0
MassimoRomano <romano93m@gmail.com>
2024-02-21 18:04:42 +0100

    fix var i with markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,9 @@ console.log('funziona')
 
 const elementList = document.getElementById("list")
 //var for the id list
+
+for (let i = 1; i<=100; i++){
+    console.log(i)  //stamp variable i
+    const markupListItem = `<li>${i}</li>`
+    console.log(markupListItem)
+}
