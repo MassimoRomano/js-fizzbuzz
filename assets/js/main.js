f0840c80d6590d05fca7c848e3f8aeebf4dd496e
MassimoRomano <romano93m@gmail.com>
2024-02-21 19:17:57 +0100

    fix markup and add else if

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,21 @@ console.log('funziona')
 const elementList = document.getElementById("list")
 //var for the id list
 
-for (let i = 1; i <= 100; i++){
-    console.log(i)  //stamp variable i
-    const markupListItem = `<li class="box box-${i}">${i}</li>`
-    console.log(markupListItem)//stamp markup list
+for (let i = 1; i <= 100; i++){  //stamp variable i
 
-    elementList.insertAdjacentHTML("beforeend", markupListItem)
-    //add sequence of numbers from 1 to 100 for markuplist
+    if (i % 3 === 0 && i % 5 === 0){
+    const multipleSame = document.createElement('li')
+    multipleSame.innerText = 'FizzBuzz'
+    elementList.append(multipleSame)
+    }
+
+    else if ( i % 3 === 0){
+        const multipleThree = document.createElement('li')
+        multipleThree.innerText = 'FIZZ'
+        elementList.append(multipleThree) 
+    }
+
+    
+
+   
 }
